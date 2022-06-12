915f4d1b3a27290ba2a2f104b7a44996cc490f81
Gregory Higley <greg@prosumma.com>
2022-06-12 04:46:36 -0400

    Some renames; README

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 # Piggy
 
-Piggy is a tiny little Haskell library that makes Database.PostgreSQL.Simple just a tiny bit easier to work with.
+Piggy is a tiny little Haskell library that makes Database.PostgreSQL.Simple just a tiny bit easier to work with. It also facilitates easier unit testing. 
 
-It uses the [Operational monad](https://hackage.haskell.org/package/operational-0.2.3.5/docs/Control-Monad-Operational.html), a simple typeclass, and a convenience newtype. The Operational monad and typeclass allow replacing database operations with stubs that do not actually talk to PostgreSQL.
+It uses the [Operational monad](https://hackage.haskell.org/package/operational-0.2.3.5/docs/Control-Monad-Operational.html), two simple typeclasses, and a convenience newtype. The Operational monad and `MonadPG` typeclass allow replacing database operations with stubs that do not actually talk to PostgreSQL.
 
 ```haskell
 class Monad m => MonadPG m where
-  type ConnectionType m
-  getConnection :: m (ConnectionType m)
-  interpretPG :: PGDSL a -> m a
+  interpret :: PGDSL a -> m a
 ```
+
+The other typeclass is `MonadConnection`:
+
+```haskell
+class Monad m => MonadConnection m where
+  getConnection :: m Connection
+```
+
+This is only needed for types which actually talk to the database, such as `PG`. This typeclass should be regarded as an implementation detail. Functions which perform database operations do not need to mention it, e.g.,
+
+```haskell
+getPatientById :: (MonadPG m, MonadThrow m) => Int -> m Patient
+getPatientById id = query1 "SELECT * FROM patient WHERE id = ?" id
+```
+
+The DSL is very simple, consisting of only 4 operations.
+
+```haskell
+data Operation a where
+  Execute :: ToRow q => Query -> q -> Operation Int64
+  Execute_ :: Query -> Operation Int64 
+  Query :: (ToRow q, FromRow r) => Query -> q -> Operation [r]
+  Query_ :: FromRow r => Query -> Operation [r]
+```
+
+This makes it very easy to write an alternate interpreter for unit tests.
--- src/Database/PostgreSQL/PG.hs
+++ src/Database/PostgreSQL/PG.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving, OverloadedStrings, TypeFamilies #-}
+{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving #-}
 
 module Database.PostgreSQL.PG where
 
@@ -8,36 +8,36 @@ import Control.Monad.Operational
 import Control.Monad.Reader
 import Data.ByteString (ByteString)
 import Data.List.Safe (head)
-import Data.Proxy (Proxy(..))
 import Database.PostgreSQL.Simple (connectPostgreSQL, fromOnly, Connection, Query, FromRow, ToRow)
 import Database.PostgreSQL.Simple.FromField (FromField)
 import qualified Database.PostgreSQL.Simple as Postgres
 import GHC.Int (Int64)
 import Prelude hiding (head)
 
-data Instruction a where
-  Execute :: ToRow q => Query -> q -> Instruction Int64
-  Execute_ :: Query -> Instruction Int64 
-  Query :: (ToRow q, FromRow r) => Query -> q -> Instruction [r]
-  Query_ :: FromRow r => Query -> Instruction [r]
+data Operation a where
+  Execute :: ToRow q => Query -> q -> Operation Int64
+  Execute_ :: Query -> Operation Int64 
+  Query :: (ToRow q, FromRow r) => Query -> q -> Operation [r]
+  Query_ :: FromRow r => Query -> Operation [r]
 
-type PGDSL a = Program Instruction a
+type PGDSL a = Program Operation a
 
 class Monad m => MonadPG m where
-  type ConnectionType m
-  getConnection :: m (ConnectionType m)
-  interpretPG :: PGDSL a -> m a
+  interpret :: PGDSL a -> m a
 
-withConnection :: MonadPG m => (ConnectionType m -> m a) -> m a
+class Monad m => MonadConnection m where
+  getConnection :: m Connection
+
+withConnection :: (MonadConnection m) => (Connection -> m a) -> m a
 withConnection f = getConnection >>= f
 
-withConn1 :: (MonadPG m, MonadIO m) => (ConnectionType m -> a -> IO b) -> a -> m b
+withConn1 :: (MonadConnection m, MonadIO m) => (Connection -> a -> IO b) -> a -> m b
 withConn1 f a = withConnection $ \conn -> liftIO $ f conn a
 
-withConn2 :: (MonadPG m, MonadIO m) => (ConnectionType m -> a -> b -> IO c) -> a -> b -> m c
+withConn2 :: (MonadConnection m, MonadIO m) => (Connection -> a -> b -> IO c) -> a -> b -> m c
 withConn2 f a b = withConnection $ \conn -> liftIO $ f conn a b
 
-interpg :: (MonadPG m, MonadIO m, ConnectionType m ~ Connection) => PGDSL a -> m a
+interpg :: (MonadConnection m, MonadIO m) => PGDSL a -> m a
 interpg m = case view m of
   Return a -> return a
   (Execute sql q) :>>= k -> withConn2 Postgres.execute sql q >>= interpg . k
@@ -51,21 +51,22 @@ connectPG :: MonadIO m => ByteString -> PG a -> m a
 connectPG connectionString pg = liftIO $ connectPostgreSQL connectionString >>= runReaderT (runPG pg) 
 
 instance MonadPG PG where 
-  type ConnectionType PG = Connection
+  interpret = interpg
+
+instance MonadConnection PG where
   getConnection = PG ask
-  interpretPG = interpg
 
 execute :: (MonadPG m, ToRow q) => Query -> q -> m Int64
-execute sql q = interpretPG $ singleton (Execute sql q)
+execute sql q = interpret $ singleton (Execute sql q)
 
 execute_ :: MonadPG m => Query -> m Int64
-execute_ sql = interpretPG $ singleton (Execute_ sql)
+execute_ sql = interpret $ singleton (Execute_ sql)
 
 query :: (MonadPG m, ToRow q, FromRow r) => Query -> q -> m [r]
-query sql q = interpretPG $ singleton (Query sql q)
+query sql q = interpret $ singleton (Query sql q)
 
 query_ :: (MonadPG m, FromRow r) => Query -> m [r]
-query_ sql = interpretPG $ singleton (Query_ sql)
+query_ sql = interpret $ singleton (Query_ sql)
 
 query1_ :: (MonadPG m, MonadThrow m, FromRow r) => Query -> m r
 query1_ sql = query_ sql >>= head
@@ -78,6 +79,3 @@ value1 sql q = fromOnly <$> query1 sql q
 
 value1_ :: (MonadPG m, MonadThrow m, FromField v) => Query -> m v
 value1_ sql = fromOnly <$> query1_ sql
-
-getFoos :: MonadPG m => m [String]
-getFoos = query_ "SELECT * FROM foo"
