a467f6358d2d27c266920fba39cc02315a5d1ad8
Gregory Higley <greg@prosumma.com>
2022-11-15 03:10:26 -0500

    More test tweaks

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,24 +1,28 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveAnyClass, DeriveGeneric #-}
 
-import Control.Monad
-import Control.Monad.IO.Class
 import Database.PostgreSQL.PG
 import Test.Hspec
-import Database.PostgreSQL.Simple (connectPostgreSQL, close, Connection, Only (Only))
+import Database.PostgreSQL.Simple (connectPostgreSQL, close, Connection, Only (Only), ToRow)
 import Database.PostgreSQL.Simple.FromRow
 import Data.List.Safe 
+import GHC.Generics (Generic)
 
 emptyListException :: Selector EmptyListException
 emptyListException = const True
 
 data Person = Person {
   personId :: !Int,
-  personLastName :: !String,
-  personFirstName :: !String
+  personFirstName :: !String,
+  personLastName :: !String
 } deriving (Show, Eq, Ord)
 
 instance FromRow Person where
   fromRow = Person <$> field <*> field <*> field
+
+data NewPerson = NewPerson {
+  newPersonFirstName :: !String,
+  newPersonLastName :: !String
+} deriving (Show, Eq, Ord, Generic, ToRow)
 
 prepareDatabase :: IO Connection 
 prepareDatabase = do
@@ -28,7 +32,7 @@ prepareDatabase = do
     execute_ "CREATE DATABASE person"
   close conn
   conn <- connectPostgreSQL "dbname=person"
-  void $ withPG conn $ do
+  withPG conn $ do
     execute_ "\
       \CREATE TABLE person(\
         \id SERIAL NOT NULL PRIMARY KEY,\
@@ -39,46 +43,49 @@ prepareDatabase = do
     \"
   return conn
 
+carl = Person 1 "Carl" "Schmitt"
+murray = Person 2 "Murray" "Rothbard"
+
+newCarl = NewPerson "Carl" "Schmitt"
+newMurray = NewPerson "Murray" "Rothbard"
+
 runTests :: Connection -> IO ()
 runTests conn = do
   hspec $ do
 
-    describe "execute_" $
-      it "runs SQL without args and returns the number of rows affected" $ do
-        flip shouldReturn 1 $ do
+    describe "execute_" $ do
+      it "runs SQL without args and returns the number of rows affected" $ 
+        flip shouldReturn 1 $ 
           withPG conn $
             execute_ "INSERT INTO person(first_name, last_name) VALUES('Carl', 'Schmitt')"
 
     describe "execute" $
-      it "runs SQL with args and returns the number of rows affected" $ do
-        flip shouldReturn 1 $ do
-          withPG conn $ do
-            let args = ("Murray", "Rothbard") :: (String, String)
-            execute "INSERT INTO person(first_name, last_name) VALUES(?, ?)" args
+      it "runs SQL with args and returns the number of rows affected" $
+        flip shouldReturn 1 $
+          withPG conn $
+            execute "INSERT INTO person(first_name, last_name) VALUES(?, ?)" newMurray 
 
     describe "query_" $
       it "queries SQL without args and returns the rows" $ do
-        let expected = [Person 2 "Murray" "Rothbard", Person 1 "Carl" "Schmitt"]
-        flip shouldReturn expected $ do 
+        flip shouldReturn [murray, carl] $ do 
           withPG conn $ 
             query_ "SELECT id, first_name, last_name FROM person ORDER BY last_name, first_name"
 
     describe "query" $
       it "queries SQL with args and returns the rows" $ do
-        flip shouldReturn [Person 2 "Murray" "Rothbard"] $ do
+        flip shouldReturn [murray] $ do
           withPG conn $ do
-            let args = ("Murray", "Rothbard") :: (String, String)
-            query "SELECT id, first_name, last_name FROM person WHERE first_name = ? and last_name = ?" args
+            query "SELECT id, first_name, last_name FROM person WHERE first_name = ? and last_name = ?" newMurray 
 
     describe "query1_" $
       it "executes a query without args and returns the first row" $ do
-        flip shouldReturn (Person 1 "Carl" "Schmitt") $ do
+        flip shouldReturn carl $ do
           withPG conn $
             query1_ "SELECT id, first_name, last_name FROM person WHERE id = 1"
 
     describe "query" $
       it "executes a query with args and returns the first row" $ do
-        flip shouldReturn (Person 2 "Murray" "Rothbard") $
+        flip shouldReturn murray $
           withPG conn $ do
             let arg = 2 :: Int
             query1 "SELECT id, first_name, last_name FROM person WHERE id = ?" (Only arg)
